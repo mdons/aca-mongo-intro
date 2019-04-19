68f1049f74e706a4766ebae649cb913f90050346
mdons <michaeljdonnell@gmail.com>
2019-04-18 19:51:42 -0500

    added atlas cluster connection

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const UserRoutes = require('./express/routes/users')
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const UserRoutes = require("./express/routes/users");
 
-mongoose.connect('mongodb://localhost/admin')
+mongoose.connect(
+  "mongodb+srv://admin:admin@cluster0-givgy.mongodb.net/test?retryWrites=true"
+);
 
-const port = process.env.PORT || 4001
-const app = express()
-app.use(bodyParser.json())
-app.use('/users', UserRoutes)
+const port = process.env.PORT || 4001;
+const app = express();
+app.use(bodyParser.json());
+app.use("/users", UserRoutes);
 
-app.get('/', (req, res) => res.send('Default route!'))
+app.get("/", (req, res) => res.send("Default route!"));
 
 app.listen(port, () => {
-  console.log(`Express app running on localhost:${port}`)
-})
+  console.log(`Express app running on localhost:${port}`);
+});
